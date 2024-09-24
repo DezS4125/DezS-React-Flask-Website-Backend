36a919769d142c893a174eb8f871cc9a8257c1c5
DezS <workingdezs@gmail.com>
2024-09-24 22:24:59 +0700

    server.py work for my table

--- server.py
+++ server.py
@@ -1,19 +1,30 @@
 from flask import Flask, jsonify
 from flask_cors import CORS
+import psycopg2
+import os
+import json
 
 app= Flask(__name__)
-cors = CORS(app, origin="http://localhost:5173")
+# cors = CORS(app, origin="http://localhost:5173")
+cors = CORS(app, origin="*")
 
-@app.route("/api/users", methods=['GET'])
+def get_db_connection():
+    conn = psycopg2.connect(host='localhost',
+                            database='mywebsite',
+                            user=os.environ['WEBSITE_DB_USERNAME'],
+                            password=os.environ['WEBSITE_DB_PASSWORD'])
+    return conn
+
+
+@app.route("/api/todo", methods=['GET'])
 def members():
-    return jsonify(
-        {
-        "users": [
-            'DezS',
-            'Cookie',
-            'Bread'
-        ]
-    })
+    conn = get_db_connection()
+    cur = conn.cursor()
+    cur.execute("select * from to_do_items")
+    to_do_items = cur.fetchall()
+    cur.close()
+    conn.close()
+    return to_do_items
 
 if __name__ =="__main__":
     app.run(debug=True, port=8080)
